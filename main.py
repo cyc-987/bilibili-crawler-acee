3ebc363f738fd8a35f5c43db5e7f6a0fd408bf29
Geniucker <wolf2003rain@163.com>
2023-06-10 10:44:48 +0800

    perf: move the setting of cookie to main.py

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-import tools.request as request
-
-
-request.setKeyword("hahahi")  # 设置关键词
+cookie = "buvid3=7EFA37D0-F1FE-F4B2-67A7-4D5CC068ADB914443infoc; b_nut=1685935714; i-wanna-go-back=-1; _uuid=E89A53FC-24A5-27C6-78C1-C54B72B7210EB14503infoc; FEED_LIVE_VERSION=V8; buvid4=AAE58FD6-2446-1F09-6FDD-67B651BF5CD015811-023060511-9TO%2BhWhxQHNJ3e7kUXAEKSOuaP1Mma70%2FihuVRGs%2FX%2BtB6fXRkR%2B%2Fw%3D%3D; SESSDATA=c80ec98f%2C1701487755%2Ca841e%2A62; bili_jct=b715433af3ed96db45c1dff797a5153d; DedeUserID=442684513; DedeUserID__ckMd5=cee8be4fc3323d8e; CURRENT_FNVAL=4048; rpdid=0zbfAHJqia|bBS0o424|12R|3w1Q60UM; fingerprint=b32f37bd83655f0c199a35a8be87bd96; buvid_fp_plain=undefined; b_ut=5; header_theme_version=CLOSE; nostalgia_conf=-1; hit-new-style-dyn=1; hit-dyn-v2=1; LIVE_BUVID=AUTO6716861403882313; CURRENT_QUALITY=120; PVID=1; buvid_fp=733b5fc5c6eec385e826338a02b1a5e0; bp_video_offset_442684513=805386254393802900; innersign=0; b_lsid=A6934EFA_188A317535C; home_feed_column=4; browser_resolution=1121-916"
 n = 5  # 最大的查询页码
 judge = "点赞数"  # 筛选依据，请从下面的field字典里的键里选
-threshold = 500000  # 被筛选字段的下限值
+threshold = 50000  # 被筛选字段的下限值
+
+if __name__ == "__main__":
+    import tools.request as request
+
+    request.setKeyword("chatgpt")  # 设置关键词
+    # 字段字典，格式为 名称:api返回值对应的键
+    field = {
+        "标题": "title",
+        "描述": "description",
+        "评论数": "review",
+        "播放数": "play",
+        "收藏数": "favorites",
+        "点赞数": "like",
+        "链接": "arcurl"
+    }
+
+    # 第一行写入字段
+    outp = open("output.csv", "w", encoding="utf-8-sig")
+    for i in field:
+        outp.write("\"{}\"".format(str(i))+",")
+    outp.write("\n")
 
 
-# 字段字典，格式为 名称:api返回值对应的键
-field = {
-    "标题": "title",
-    "描述": "description",
-    "评论数": "review",
-    "播放数": "play",
-    "收藏数": "favorites",
-    "点赞数": "like",
-    "链接": "arcurl"
-}
-
-# 第一行写入字段
-outp = open("output.csv", "w", encoding="utf-8-sig")
-for i in field:
-    outp.write("\"{}\"".format(str(i))+",")
-outp.write("\n")
-
-
-for i in range(1, n+1):
-    data = request.getData(i)
-    result = {}
-    # 便利每页个条目
-    for item in data:
-        # 获取每一个字段的值
-        for j in field:
-            result[j] = str(item.get(field[j])).replace("\r", "").replace("\n", "\\n")
-        # 若符合条件则写入
-        if int(result[judge]) > threshold:
-            for j in result.values():
-                outp.write("\"{}\"".format(j)+",")
-            outp.write("\n")
-outp.close()
+    for i in range(1, n+1):
+        data = request.getData(i)
+        if data:
+            result = {}
+            # 便利每页个条目
+            for item in data:
+                # 获取每一个字段的值
+                for j in field:
+                    result[j] = str(item.get(field[j])).replace("\r", "").replace("\n", "\\n")
+                # 若符合条件则写入
+                if int(result[judge]) > threshold:
+                    for j in result.values():
+                        outp.write("\"{}\"".format(j)+",")
+                    outp.write("\n")
+    outp.close()
--- tools/request.py
+++ tools/request.py
@@ -1,10 +1,11 @@
 import requests
+from main import cookie
 
 
 # headers，包括了UA和Cookie
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41",
-    "Cookie": "buvid3=456845EF-E2DC-E75A-35C3-FE69F0BE7BFC10922infoc; b_nut=1672140010; _uuid=B159A93D-B667-67C1-49C7-99659134B72C12043infoc; buvid4=B2A40E96-162B-2CEB-42BA-01AB6ABADCB012661-022122719-mQ3ahp215nlp41kZvUuzCA%3D%3D; buvid_fp_plain=undefined; SESSDATA=0e123f1b%2C1687692218%2Cafc50%2Ac2; bili_jct=ca97d687f4bc7f72e89f8bc2152a1689; DedeUserID=442684513; DedeUserID__ckMd5=cee8be4fc3323d8e; sid=5auof2ki; CURRENT_FNVAL=4048; rpdid=|(J|)|lYukJR0J'uY~kuR|Jkl; i-wanna-go-back=-1; b_ut=5; nostalgia_conf=-1; fingerprint=803313416b7f5e22db4ee7eb805b00e6; buvid_fp=4f72c9d867a9103dae4f20f0188bf1d2; CURRENT_QUALITY=116; LIVE_BUVID=AUTO6416749602193257; hit-new-style-dyn=0; hit-dyn-v2=1; header_theme_version=CLOSE; home_feed_column=5; innersign=1; bp_video_offset_442684513=774413110852190300; b_lsid=681078F7B_186F54A994F; PVID=4"
+    "Cookie": cookie
 }
 # 抓包得到的api
 api = "https://api.bilibili.com/x/web-interface/wbi/search/type"
